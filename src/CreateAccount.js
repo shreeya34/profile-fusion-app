de9966e5268ab0838cad39e1f9c25dab5f4a9ee4
shreeya34 <1shreeya2@gmail.com>
2025-02-14 18:57:56 +0545

    work on handle username exists

--- src/CreateAccount.js
+++ src/CreateAccount.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';  // Ensure this is imported correctly
-import './CreateAccount.css'; // Styling the page
+import './CreateAccount.css'; 
  
 
 
